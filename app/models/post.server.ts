805e0ad97353e055e62f178ae63444a9d7d9e0c1
Arturo Chen <art.can.fly@gmail.com>
2022-03-29 21:51:41 -0400

    move posts to prisma

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,14 @@
-type Post = {
+import { prisma } from "~/db.server";
+
+export type Post = {
   slug: string;
   title: string;
 };
 
 export async function getPosts(): Promise<Post[]> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtap I made Just For You",
-    },
-  ];
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
 }
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -16,8 +16,8 @@ export default function Posts() {
   const { posts } = useLoaderData() as LoaderData;
 
   return (
-    <main>
-      <h1>Posts</h1>
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">Posts</h1>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
